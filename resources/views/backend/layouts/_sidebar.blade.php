ccaf235f99710b4d20d22cc83bcdd9f6f117a3b3
RestiaGnoth <thongphamminh3@gmail.com>
2025-02-21 10:06:46 +0700

    refactor: simplify sidebar menu structure and remove unused breadcrumb navigation

--- resources/views/backend/layouts/_sidebar.blade.php
+++ resources/views/backend/layouts/_sidebar.blade.php
@@ -1,185 +1,85 @@
+@php
+    $menuItems = [
+        ['route' => 'dashboard', 'icon' => 'fa-home', 'label' => 'Bảng điều khiển'],
+        ['route' => 'employees', 'icon' => 'fa-users', 'label' => 'Nhân viên'],
+        ['route' => 'jobs', 'icon' => 'fa-briefcase', 'label' => 'Công việc'],
+        ['route' => 'job_history', 'icon' => 'fa-history', 'label' => 'Lịch sử công việc'],
+        ['route' => 'job_grades', 'icon' => 'fa-star', 'label' => 'Cấp bậc công việc'],
+        ['route' => 'regions', 'icon' => 'fa-asterisk', 'label' => 'Vùng miền'],
+        ['route' => 'countries', 'icon' => 'fa-flag', 'label' => 'Quốc gia'],
+        ['route' => 'locations', 'icon' => 'fa-map-marker-alt', 'label' => 'Địa điểm'],
+        ['route' => 'departments', 'icon' => 'fa-building', 'label' => 'Phòng ban'],
+        ['route' => 'manager', 'icon' => 'fa-user', 'label' => 'Quản lý'],
+    ];
+@endphp
+
 <!-- Preloader -->
 <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="{{ url('public/backend/dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo"
-        height="60" width="60">
+    <img class="animation__shake" src="{{ asset('backend/dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60"
+        width="60">
 </div>
 
 <!-- Navbar -->
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
     <ul class="navbar-nav">
         <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            @foreach ($segments as $key => $segment)
-        <li class="breadcrumb-item mt-2">
-            <a href="{{ '/' . implode('/', array_slice($segments, 0, $key + 1)) }}">
-                {{ ucfirst($segment) }}
-            </a>
-        </li>
+        @foreach ($segments as $key => $segment)
+            <li class="breadcrumb-item mt-2">
+                <a href="{{ '/' . implode('/', array_slice($segments, 0, $key + 1)) }}">
+                    {{ ucfirst($segment) }}
+                </a>
+            </li>
         @endforeach
-        </li>
-
     </ul>
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
-        <!-- Navbar Search -->
         <li class="nav-item">
             <a class="nav-link" href="{{ url('logout') }}">
                 <i class="fas fa-sign-out-alt"></i>
             </a>
-
         </li>
-
-
     </ul>
 </nav>
 <!-- /.navbar -->
 
-<!-- Main Sidebar Container -->
+<!-- Sidebar -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-
+    <a href="{{ url('admin/dashboard') }}" class="brand-link">
         <img src="{{ asset('backend/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
-
-        <span class="brand-text font-weight-light">HR </span>
+        <span class="brand-text font-weight-light">System</span>
     </a>
 
-    <!-- Sidebar -->
     <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-
                 <img src="{{ asset('backend/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
                     alt="User Image">
-
             </div>
             <div class="info">
-                <a href="" class="d-block">{{ Auth::user()->name }}</a>
+                <a href="#" class="d-block">{{ Auth::user()->name }}</a>
             </div>
         </div>
-
-
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                 data-accordion="false">
-                <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
 
-
-                <li class="nav-item">
-
-                    <a href="{{ url('admin/dashboard') }}"
-                        class="nav-link @if (Request::segment(2) == 'dashboard') active @endif">
-
-                        <i class="nav-icon fa fa-home"></i>
-                        <p>
-                            Dashboard
-                        </p>
-                    </a>
-                </li>
-
-                <li class="nav-item">
-
-                    <a href="{{ url('admin/employees') }}"
-                        class="nav-link @if (Request::segment(2) == 'employees') active @endif">
-
-                        <i class="nav-icon fa fa-users"></i>
-                        <p>
-                            Employees
-                        </p>
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    <a href="{{ url('admin/jobs') }}" class="nav-link @if (Request::segment(2) == 'jobs') active @endif">
-                        <i class="nav-icon fa fa-briefcase"></i>
-                        <p>
-                            Jobs
-                        </p>
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    <a href="{{ url('admin/job_history') }}" class="nav-link">
-                        <i class="nav-icon fa fa-history"></i>
-                        <p>
-                            Job History
-                        </p>
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    <a href="{{ url('admin/job_grades') }}"
-                        class="nav-link @if (Request::segment(2) == 'job_grades') active @endif">
-                        <i class="nav-icon fa fa-star"></i>
-                        <p>
-                            Job Grades
-                        </p>
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    <a href="{{ url('admin/regions') }}"
-                        class="nav-link @if (Request::segment(2) == 'regions') active @endif">
-                        <i class="nav-icon fa fa-asterisk"></i>
-                        <p>
-                            Regions
-                        </p>
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    <a href="{{ url('admin/countries') }}"
-                        class="nav-link @if (Request::segment(2) == 'countries') active @endif">
-                        <i class="nav-icon fa fa-flag"></i>
-                        <p>
-                            Countries
-                        </p>
-                    </a>
-                </li>
-
-                <li class="nav-item">
-
-                    <a href="{{ url('admin/locations') }}"
-                        class="nav-link  @if (Request::segment(2) == 'locations') active @endif">
-                        <i class="nav-icon fa fa-map-marker-alt"></i>
-                        <p>
-                            Locations
-                        </p>
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    <a href="{{ url('admin/departments') }}"
-                        class="nav-link @if (Request::segment(2) == 'departments') active @endif">
-                        <i class="nav-icon fa fa-building"></i>
-                        <p>
-                            Department
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ url('admin/manager') }}"
-                        class="nav-link @if (Request::segment(2) == 'manager') active @endif">
-                        <i class="nav-icon fa fa-user"></i>
-                        <p>Manager</p>
-                    </a>
-                </li>
-
-
+                @foreach ($menuItems as $item)
+                    <li class="nav-item">
+                        <a href="{{ url('admin/' . $item['route']) }}"
+                            class="nav-link @if (Request::segment(2) == $item['route']) active @endif">
+                            <i class="nav-icon fa {{ $item['icon'] }}"></i>
+                            <p>{{ $item['label'] }}</p>
+                        </a>
+                    </li>
+                @endforeach
 
             </ul>
         </nav>
-        <!-- /.sidebar-menu -->
-
     </div>
-    <!-- /.sidebar -->
 </aside>
